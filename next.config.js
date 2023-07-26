5f2a279409025bd6452c554ba4415b1b81631eae
WONILLISM <wonillism@gmail.com>
2023-07-26 18:00:18 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `https://nextjs-chat-brggj11u9-wonillism.vercel.app/:path*`,
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
