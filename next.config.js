fd2f18672a4fefdb34e682ae302ef61f73591442
WONILLISM <wonillism@gmail.com>
2023-07-26 18:27:53 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.externals.push({
-      "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil",
-    });
-
-    return config;
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://nextjs-chat-app-flax.vercel.app/:path*",
+      },
+    ];
   },
 };
 
